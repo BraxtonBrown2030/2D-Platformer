7fafa0bacc1acee4c28436bfde3455c60a9cf59b
Braxton Brown <10944268@uvu.edu>
2023-08-29 23:36:04 -0600

    2D Game First Save and Creation

---
+++ 2D platformer/Assets/Sctipts/PlayerMovment.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+    
+    [Header("Player Componts")]
+    private Transform player;
+
+    void Start()
+    {
+        
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
