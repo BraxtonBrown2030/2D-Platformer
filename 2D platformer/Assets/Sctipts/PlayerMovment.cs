fbb1d3064f759e817b8c9b634efece359d51967b
Braxton Brown <10944268@uvu.edu>
2023-08-29 23:59:49 -0600

    scene and script set up

--- 2D platformer/Assets/Sctipts/PlayerMovment.cs
+++ 2D platformer/Assets/Sctipts/PlayerMovment.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour
 {
     
     [Header("Player Componts")]
-    private Transform player;
+    private Rigidbody2D ts;
+
+    [Header("jumpvalue")]
+    [SerializeField] private int jumpvalue;
 
     void Start()
     {
         
-        
+        ts = GetComponent<Rigidbody2D>();
 
 
     }
@@ -19,6 +23,18 @@ public class PlayerMovment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Jump();
+    }
+
+    void Jump()
+    {
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+
+            ts.position = new Vector2(transform.position.x, jumpvalue);
+
+        }
+
     }
 }
